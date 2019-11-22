b62314d26241ecb95e0eedd011d292f94692871f
wang-jc <726535498@qq.com>
2019-11-22 13:31:49 +0800

    缓存

---
+++ src/main/java/com/atguigu/cache/service/DeptService.java
@@ -0,0 +1,11 @@
+package com.atguigu.cache.service;
+
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class DeptService {
+
+
+}
---
+++ src/test/java/com/atguigu/cache/SpringbootCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.cache;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
